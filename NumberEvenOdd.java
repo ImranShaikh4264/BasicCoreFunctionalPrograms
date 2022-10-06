5f8992706a69e9fbd22f50db4ccd0c4c3d9bfa7b
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-10-06 22:26:01 +0530

    Added: Java program to check given number is Even or Odd

---
+++ NumberEvenOdd.java
@@ -0,0 +1,14 @@
+package Day5;
+
+public class NumberEvenOdd {
+	public static void main(String[] args) {
+		int Number = 51;
+		if ((Number%2)== 0){
+		System.out.println("Number is Even");
+		}
+		else {
+			System.out.println("Number is Odd");
+		}
+	}
+
+}
